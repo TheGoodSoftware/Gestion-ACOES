80db5dfa883a2e28ebd2d4f599cc006340ec43cb
M8anu <manudoon_2012@hotmail.com>
2019-01-09 16:11:56 +0100

    "VISIBLE" words removed from the script, aswell as the inconsistencies regarding the relationships between some entities, also changed one of the attributes in the relationship between 'Usuario' and 'Rol'.

--- bd/scriptACOES.sql
+++ bd/scriptACOES.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`apadrinar` (
   `NINO_idNen` INT(10) NOT NULL,
   `USUARIO_idUsuario` INT(10) NOT NULL,
   PRIMARY KEY (`NINO_idNen`, `USUARIO_idUsuario`),
-  INDEX `fk_NIÑO_has_USUARIO_USUARIO1` (`USUARIO_idUsuario` ASC) VISIBLE,
+  INDEX `fk_NIÑO_has_USUARIO_USUARIO1` (`USUARIO_idUsuario` ASC),
   CONSTRAINT `fk_NIÑO_has_USUARIO_NIÑO1`
     FOREIGN KEY (`NINO_idNen`)
     REFERENCES `mydb`.`nino` (`idNen`),
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`asociacion` (
   `Nombre` VARCHAR(500) NOT NULL,
   `Localizacion` VARCHAR(450) NULL DEFAULT NULL,
   PRIMARY KEY (`Nombre`),
-  UNIQUE INDEX `idAs_UNIQUE` (`Nombre` ASC) VISIBLE)
+  UNIQUE INDEX `idAs_UNIQUE` (`Nombre` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -60,8 +60,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`contenedor` (
   `Enviado` TINYINT(4) NULL DEFAULT '0',
   `USUARIO_idUsuario` INT(10) NOT NULL,
   PRIMARY KEY (`idCont`, `USUARIO_idUsuario`),
-  UNIQUE INDEX `idCont_UNIQUE` (`idCont` ASC) VISIBLE,
-  INDEX `fk_CONTENEDOR_USUARIO2` (`USUARIO_idUsuario` ASC) VISIBLE,
+  UNIQUE INDEX `idCont_UNIQUE` (`idCont` ASC),
+  INDEX `fk_CONTENEDOR_USUARIO2` (`USUARIO_idUsuario` ASC),
   CONSTRAINT `fk_CONTENEDOR_USUARIO2`
     FOREIGN KEY (`USUARIO_idUsuario`)
     REFERENCES `mydb`.`usuario` (`idUsuario`))
@@ -81,9 +81,9 @@ CREATE TABLE IF NOT EXISTS `mydb`.`donacion` (
   `GESTION ECONOMICA_idBalance` INT(10) NOT NULL,
   `USUARIO_idUsuario` INT(10) NOT NULL,
   PRIMARY KEY (`idDon`, `GESTION ECONOMICA_idBalance`),
-  UNIQUE INDEX `idDon_UNIQUE` (`idDon` ASC) VISIBLE,
-  INDEX `fk_DONACIÓN_GESTIÓN ECONÓMICA1` (`GESTION ECONOMICA_idBalance` ASC) VISIBLE,
-  INDEX `fk_DONACIÓN_USUARIO1` (`USUARIO_idUsuario` ASC) VISIBLE,
+  UNIQUE INDEX `idDon_UNIQUE` (`idDon` ASC),
+  INDEX `fk_DONACIÓN_GESTIÓN ECONÓMICA1` (`GESTION ECONOMICA_idBalance` ASC),
+  INDEX `fk_DONACIÓN_USUARIO1` (`USUARIO_idUsuario` ASC),
   CONSTRAINT `fk_DONACIÓN_GESTIÓN ECONÓMICA1`
     FOREIGN KEY (`GESTION ECONOMICA_idBalance`)
     REFERENCES `mydb`.`gestionEconomica` (`idBalance`),
@@ -106,9 +106,9 @@ CREATE TABLE IF NOT EXISTS `mydb`.`gasto` (
   `GESTION ECONOMICA_idBalance` INT(10) NOT NULL,
   `USUARIO_idUsuario` INT(10) NOT NULL,
   PRIMARY KEY (`idGasto`, `GESTION ECONOMICA_idBalance`, `USUARIO_idUsuario`),
-  UNIQUE INDEX `idGasto_UNIQUE` (`idGasto` ASC) VISIBLE,
-  INDEX `fk_GASTO_GESTIÓN ECONÓMICA1` (`GESTION ECONOMICA_idBalance` ASC) VISIBLE,
-  INDEX `fk_GASTO_USUARIO2` (`USUARIO_idUsuario` ASC) VISIBLE,
+  UNIQUE INDEX `idGasto_UNIQUE` (`idGasto` ASC),
+  INDEX `fk_GASTO_GESTIÓN ECONÓMICA1` (`GESTION ECONOMICA_idBalance` ASC),
+  INDEX `fk_GASTO_USUARIO2` (`USUARIO_idUsuario` ASC),
   CONSTRAINT `fk_GASTO_GESTIÓN ECONÓMICA1`
     FOREIGN KEY (`GESTION ECONOMICA_idBalance`)
     REFERENCES `mydb`.`gestionEconomica` (`idBalance`),
@@ -127,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`gestionEconomica` (
   `idBalance` INT(10) NOT NULL,
   `balanceTotal` DOUBLE NOT NULL,
   PRIMARY KEY (`idBalance`),
-  UNIQUE INDEX `idBalance_UNIQUE` (`idBalance` ASC) VISIBLE)
+  UNIQUE INDEX `idBalance_UNIQUE` (`idBalance` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -141,8 +141,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`nino` (
   `Edad` INT(2) NOT NULL,
   `PROYECTO_idProy` INT(10) NOT NULL,
   PRIMARY KEY (`idNen`),
-  UNIQUE INDEX `idNen_UNIQUE` (`idNen` ASC) VISIBLE,
-  INDEX `fk_NINO_PROYECTO1` (`PROYECTO_idProy` ASC) VISIBLE,
+  UNIQUE INDEX `idNen_UNIQUE` (`idNen` ASC),
+  INDEX `fk_NINO_PROYECTO1` (`PROYECTO_idProy` ASC),
   CONSTRAINT `fk_NINO_PROYECTO1`
     FOREIGN KEY (`PROYECTO_idProy`)
     REFERENCES `mydb`.`proyecto` (`idProy`),
@@ -163,8 +163,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`notas` (
   `Curso` VARCHAR(500) NOT NULL DEFAULT 'Primaria',
   `nino_idNen` INT(10) NOT NULL,
   PRIMARY KEY (`idNotasNen`),
-  UNIQUE INDEX `idNotasNen_UNIQUE` (`idNotasNen` ASC) VISIBLE,
-  INDEX `fk_notas_nino1_idx` (`nino_idNen` ASC) VISIBLE,
+  UNIQUE INDEX `idNotasNen_UNIQUE` (`idNotasNen` ASC),
+  INDEX `fk_notas_nino1_idx` (`nino_idNen` ASC),
   CONSTRAINT `fk_notas_nino1`
     FOREIGN KEY (`nino_idNen`)
     REFERENCES `mydb`.`nino` (`idNen`)
@@ -185,9 +185,9 @@ CREATE TABLE IF NOT EXISTS `mydb`.`objeto` (
   `CONTENEDOR_idCont` INT(10) NOT NULL,
   `USUARIO_idUsuario` INT(10) NOT NULL,
   PRIMARY KEY (`idObjeto`, `CONTENEDOR_idCont`),
-  UNIQUE INDEX `idObjeto_UNIQUE` (`idObjeto` ASC) VISIBLE,
-  INDEX `fk_OBJETO_CONTENEDOR1` (`CONTENEDOR_idCont` ASC) VISIBLE,
-  INDEX `fk_OBJETO_USUARIO2` (`USUARIO_idUsuario` ASC) VISIBLE,
+  UNIQUE INDEX `idObjeto_UNIQUE` (`idObjeto` ASC),
+  INDEX `fk_OBJETO_CONTENEDOR1` (`CONTENEDOR_idCont` ASC),
+  INDEX `fk_OBJETO_USUARIO2` (`USUARIO_idUsuario` ASC),
   CONSTRAINT `fk_OBJETO_CONTENEDOR1`
     FOREIGN KEY (`CONTENEDOR_idCont`)
     REFERENCES `mydb`.`contenedor` (`idCont`),
@@ -206,7 +206,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`permiso` (
   `ID` INT(10) NOT NULL,
   `Descripcion` VARCHAR(500) NOT NULL,
   PRIMARY KEY (`ID`),
-  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE)
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -222,7 +222,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`persona` (
   `Direccion` VARCHAR(45) NOT NULL,
   `Pueblo` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idPersona`),
-  UNIQUE INDEX `idPersona_UNIQUE` (`idPersona` ASC) VISIBLE)
+  UNIQUE INDEX `idPersona_UNIQUE` (`idPersona` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -239,7 +239,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`proyecto` (
   `Localizacion` VARCHAR(500) NOT NULL,
   `Acabado` TINYINT(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`idProy`),
-  UNIQUE INDEX `idProy_UNIQUE` (`idProy` ASC) VISIBLE)
+  UNIQUE INDEX `idProy_UNIQUE` (`idProy` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -252,7 +252,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`responsableDe` (
   `USUARIO_idUsuario` INT(10) NOT NULL,
   `PROYECTO_idProy` INT(10) NOT NULL,
   PRIMARY KEY (`USUARIO_idUsuario`, `PROYECTO_idProy`),
-  INDEX `fk_USUARIO_has_PROYECTO_PROYECTO1` (`PROYECTO_idProy` ASC) VISIBLE,
+  INDEX `fk_USUARIO_has_PROYECTO_PROYECTO1` (`PROYECTO_idProy` ASC),
   CONSTRAINT `fk_USUARIO_has_PROYECTO_PROYECTO1`
     FOREIGN KEY (`PROYECTO_idProy`)
     REFERENCES `mydb`.`proyecto` (`idProy`),
@@ -273,11 +273,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`rol` (
   `Pais` VARCHAR(500) NOT NULL DEFAULT 'Espana',
   `Descripcion` VARCHAR(500) NULL DEFAULT NULL,
   PRIMARY KEY (`idRol`),
-  UNIQUE INDEX `Nombre_UNIQUE` (`Nombre` ASC) VISIBLE,
-  UNIQUE INDEX `idRol_UNIQUE` (`idRol` ASC) VISIBLE,
-  CONSTRAINT `refIdProy`
-    FOREIGN KEY (`idRol`)
-    REFERENCES `mydb`.`proyecto` (`idProy`))
+  UNIQUE INDEX `Nombre_UNIQUE` (`Nombre` ASC),
+  UNIQUE INDEX `idRol_UNIQUE` (`idRol` ASC),
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -290,8 +287,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`rolesPermisos` (
   `PERMISO_ID` INT(10) NOT NULL,
   `ROL_idRol` INT(10) NOT NULL,
   PRIMARY KEY (`PERMISO_ID`, `ROL_idRol`),
-  UNIQUE INDEX `PERMISO_ID_UNIQUE` (`PERMISO_ID` ASC) VISIBLE,
-  INDEX `fk_PERMISO_has_ROL_ROL1` (`ROL_idRol` ASC) VISIBLE,
+  UNIQUE INDEX `PERMISO_ID_UNIQUE` (`PERMISO_ID` ASC),
+  INDEX `fk_PERMISO_has_ROL_ROL1` (`ROL_idRol` ASC),
   CONSTRAINT `fk_PERMISO_has_ROL_PERMISO1`
     FOREIGN KEY (`PERMISO_ID`)
     REFERENCES `mydb`.`permiso` (`ID`),
@@ -313,13 +310,13 @@ CREATE TABLE IF NOT EXISTS `mydb`.`usuario` (
   `ASOCIACION_Nombre` VARCHAR(500) NOT NULL,
   `ROL_idRol` INT(10) NOT NULL,
   PRIMARY KEY (`idUsuario`, `ROL_idRol`),
-  UNIQUE INDEX `idUsuario_UNIQUE` (`idUsuario` ASC) VISIBLE,
-  INDEX `fk_USUARIO_ASOCIACIÓN1` (`ASOCIACION_Nombre` ASC) VISIBLE,
+  UNIQUE INDEX `idUsuario_UNIQUE` (`idUsuario` ASC),
+  INDEX `fk_USUARIO_ASOCIACIÓN1` (`ASOCIACION_Nombre` ASC),
   CONSTRAINT `fk_USUARIO_ASOCIACIÓN1`
     FOREIGN KEY (`ASOCIACION_Nombre`)
     REFERENCES `mydb`.`asociacion` (`Nombre`),
   CONSTRAINT `fk_USUARIO_ROL1`
-    FOREIGN KEY (`idUsuario`)
+    FOREIGN KEY (`ROL_idRol`)
     REFERENCES `mydb`.`rol` (`idRol`),
   CONSTRAINT `idPersUs`
     FOREIGN KEY (`idUsuario`)
